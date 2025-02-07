d3248def95824f6777696cc17f49ad55cdf567ec
정성빈 <tjdqls3124@gmail.com>
2025-02-07 14:25:57 +0900

    코테

--- baekjoon/beakjoon.java
+++ baekjoon/beakjoon.java
@@ -1,9 +1,7 @@
-package basic;
+package baekjoon;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Scanner;
 
 public class beakjoon {
     public static void main(String[] args) {
---
+++ baekjoon/main.java
@@ -0,0 +1,15 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+
+        int[] arr = new int[n]; // 입력값 크기의 배열 생성
+
+        }
+}
+
