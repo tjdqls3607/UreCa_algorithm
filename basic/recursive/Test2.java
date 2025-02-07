2141f562c30d4c727eb8db2cc19c346ae67978bd
정성빈 <tjdqls3124@gmail.com>
2025-02-07 11:20:07 +0900

    팩토리얼 계산

--- basic/recursive/Test2.java
+++ basic/recursive/Test2.java
@@ -4,6 +4,8 @@ package basic.recursive;
 public class Test2 {
     public static void main(String[] args) {
         factorial(5);
+        int result = factorial2(5);
+        System.out.println(result);
     }
 
     // #1. 결과값을 static 변수
@@ -19,5 +21,15 @@ public class Test2 {
 
         // 재귀호출
         factorial(n-1);
+    }
+
+    // #2. 결과값을 재귀호출의 return 값으로
+    // 바닥부터 계산되어서 bottom up 으로 처리
+    static int factorial2(int n){
+        // 기저조건
+        if(n==1) return 1;
+        // n == 3 일 때
+        // 2 X 1의 결과에 자신 3을 곱해서 4에게 리턴
+        return n*factorial2(n-1);
     }
 }
