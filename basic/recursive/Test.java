3fd54f2af25cee4d357fe94c1e280f1417174c45
정성빈 <tjdqls3124@gmail.com>
2025-02-07 10:45:58 +0900

    재귀호출

--- basic/recursive/Test.java
+++ basic/recursive/Test.java
@@ -3,7 +3,9 @@ package basic.recursive;
 public class Test {
     public static void main(String[] args) {
 //        m1();
-        m1_param(0);
+//        m1_param(0);
+//        m2();
+        m3();
     }
 
     //// 변수
@@ -22,5 +24,34 @@ public class Test {
         // 파라미터 변수 <= 재귀호출마다 스택에 새로 만들어지고 이전 호출에서 전달된 값을 복사
         System.out.println("m1_param() " + i++);
         m1_param(i);
+    }
+
+    //// 기저 조건
+    static  int m2_cnt = 5;
+    static void m2() {
+        System.out.println("앞 m2() " + m2_cnt);
+        // 기저조건
+        if (m2_cnt > 0) {
+            m2_cnt--;
+            m2();
+        }
+        System.out.println("뒤 m2() " + m2_cnt);
+
+    }
+
+    // 5번 호출
+    static  int m3_cnt = 5;
+    static void m3() {
+        // 기저조건
+        if (m3_cnt == 0) return;
+
+        System.out.println("앞 m3() " + m3_cnt);
+
+        m3_cnt--;
+        m3();
+        m3_cnt++;   // 이 코드가 없으면 static 변수 m3_cnt가 0으로 감소한 후 계속 0 으로 유지, 원상복구가 필요함
+
+        System.out.println("뒤 m3() " + m3_cnt);
+
     }
 }
