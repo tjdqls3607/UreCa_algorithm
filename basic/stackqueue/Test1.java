b9f1942893a4e8e4b2d43caca04f66d544bf6f20
정성빈 <tjdqls3124@gmail.com>
2025-02-10 10:42:25 +0900

    데크 사용한 스택

--- basic/stackqueue/Test1.java
+++ basic/stackqueue/Test1.java
@@ -1,31 +1,61 @@
 package basic.stackqueue;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Stack;
 
 public class Test1 {
     public static void main(String[] args) {
         // stack
-        Stack<Integer> stack = new Stack<>();
+//        {
+//            Stack<Integer> stack = new Stack<>();
+//
+//            stack.push(3);
+//            System.out.println(stack.peek());
+//            System.out.println(stack.size());
+//
+//            stack.push(1);
+//            System.out.println(stack.peek());
+//            System.out.println(stack.size());
+//
+//            stack.push(7);
+//            System.out.println(stack.peek());
+//            System.out.println(stack.size());
+//
+//            Integer num = stack.pop();
+//            System.out.println(num);
+//            System.out.println(stack.peek());
+//            System.out.println(stack.size());
+//
+//            while (!stack.isEmpty()) {
+//                System.out.print(stack.pop() + " ");
+//            }
+//        }
 
-        stack.push(3);
-        System.out.println(stack.peek());
-        System.out.println(stack.size());
+        // stack using Deque
+        {
+            Deque<Integer> stack = new ArrayDeque<>();
 
-        stack.push(1);
-        System.out.println(stack.peek());
-        System.out.println(stack.size());
+            stack.push(3);
+            System.out.println(stack.peek());
+            System.out.println(stack.size());
 
-        stack.push(7);
-        System.out.println(stack.peek());
-        System.out.println(stack.size());
+            stack.push(1);
+            System.out.println(stack.peek());
+            System.out.println(stack.size());
 
-        Integer num = stack.pop();
-        System.out.println(num);
-        System.out.println(stack.peek());
-        System.out.println(stack.size());
+            stack.push(7);
+            System.out.println(stack.peek());
+            System.out.println(stack.size());
 
-        while (!stack.isEmpty()) {
-        System.out.print(stack.pop() + " ");
+            Integer num = stack.pop();
+            System.out.println(num);
+            System.out.println(stack.peek());
+            System.out.println(stack.size());
+
+            while (!stack.isEmpty()) {
+                System.out.print(stack.pop() + " ");
+            }
         }
     }
 }
