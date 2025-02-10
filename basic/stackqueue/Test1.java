3d92ae48f1b082a60077b49bd7a5a47d800a4e34
정성빈 <tjdqls3124@gmail.com>
2025-02-10 10:34:05 +0900

    스택

---
+++ basic/stackqueue/Test1.java
@@ -0,0 +1,13 @@
+package basic.stackqueue;
+
+import java.util.Stack;
+
+public class Test1 {
+    public static void main(String[] args) {
+        // stack
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(3);
+        System.out.println(stack.peek());
+    }
+}
