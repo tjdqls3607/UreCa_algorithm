8ddd9e962cc2e02ad22b2cd19ecb3b4dcbc0e65e
정성빈 <tjdqls3124@gmail.com>
2025-02-10 11:12:22 +0900

    사용자 정의 클래스를 stack, queue 활용

--- basic/stackqueue/Test3.java
+++ basic/stackqueue/Test3.java
@@ -2,26 +2,45 @@ package basic.stackqueue;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.Queue;
 
 public class Test3 {
     public static void main(String[] args) {
         // 사용자 정의 클래스를 stack, queue 활용
 
         // stack
+//        {
+//            Deque<Node> stack = new ArrayDeque<Node>();
+//            stack.push(new Node(3, 0, 4));
+//            stack.push(new Node(2, 8, 1));
+//            stack.push(new Node(1, 6, 3));
+//            stack.push(new Node(5, 5, 10));
+//
+//            System.out.println(stack.pop());
+//            System.out.println(stack.peek());
+//
+//            stack.push(new Node(9, 0, 3));
+//
+//            while (!stack.isEmpty()) {
+//                System.out.print(stack.pop() + " ");
+//            }
+//        }
+
+        // queue
         {
-            Deque<Node> stack = new ArrayDeque<Node>();
-            stack.push(new Node(3, 0, 4));
-            stack.push(new Node(2, 8, 1));
-            stack.push(new Node(1, 6, 3));
-            stack.push(new Node(5, 5, 10));
+            Queue<Node> queue = new ArrayDeque<>();
+            queue.offer(new Node(3, 0, 4));
+            queue.offer(new Node(2, 8, 1));
+            queue.offer(new Node(1, 6, 3));
+            queue.offer(new Node(5, 5, 10));
 
-            System.out.println(stack.pop());
-            System.out.println(stack.peek());
+            System.out.println(queue.poll());
+            System.out.println(queue.peek());
 
-            stack.push(new Node(9, 0, 3));
+            queue.offer(new Node(9, 0, 3));
 
-            while (!stack.isEmpty()) {
-                System.out.print(stack.pop() + " ");
+            while (!queue.isEmpty()) {
+                System.out.print(queue.poll() + " ");
             }
         }
     }
