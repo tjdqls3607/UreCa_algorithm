c738f49ce333a5767d86c520dc8b0b4489f4a52d
정성빈 <tjdqls3124@gmail.com>
2025-02-24 14:11:48 +0900

    택배상자 꺼내기

---
+++ pr/pr/택배상자꺼내기.java
@@ -0,0 +1,12 @@
+package pr;
+
+class pr.택배상자꺼내기 {
+    public int solution(int n, int w, int num) {
+        int answer = 0;
+        while (num <= n) {
+            num += 2 * (w - ((num - 1) % w)-1) +1;
+            answer++;
+        }
+        return answer;
+    }
+}
