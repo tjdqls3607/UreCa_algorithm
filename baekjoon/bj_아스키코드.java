5f20ae82757a12c45e07be0a2a049d46c06b3b28
정성빈 <tjdqls3124@gmail.com>
2025-03-13 16:09:26 +0900

    백준 브론즈 문제

---
+++ baekjoon/bj_아스키코드.java
@@ -0,0 +1,14 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class bj_아스키코드 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char a = sc.next().charAt(0);
+        int n = (int) a;
+        System.out.println(n);
+
+    }
+}
