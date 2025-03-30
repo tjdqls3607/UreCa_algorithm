656753807e097508284696a3960dd8c8862d3ef9
정성빈 <tjdqls3124@gmail.com>
2025-03-30 16:30:24 +0900

    백준 브론즈 문제

---
+++ baekjoon/bj_AplusB_5.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class bj_AplusB_5 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if (a == 0 && b == 0) break;
+            System.out.println(a + b);
+        }
+    }
+}
